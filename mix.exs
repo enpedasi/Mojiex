948a373ca801b771c05f263faf706049f746b991
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2018-07-22 14:35:02 +0900

    Do mix format

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Mojiex.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:credo, "~> 0.9.3", runtime: false, only: [:dev, :test]},
-      {:power_assert, "~> 0.0"}      
+      {:power_assert, "~> 0.0"}
     ]
   end
 end
